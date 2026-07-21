522266a0bd257ec3b96004bef027d5c2db7db09d
Wesley Evans <162752827+evans-rxfire@users.noreply.github.com>
2026-07-21 18:25:35 -0500

    2026.07.21 Adding huisache data to Brush Busters reference file.

--- js/brushBustersReference.js
+++ js/brushBustersReference.js
@@ -1,5 +1,46 @@
 export const brushBustersRecomendations = [
         {
+        plantName: ["huisache"],
+        source: {
+            title: "How to Beat Huisache: Individual Plant Treatment Leaf and Stem Applications",
+            publicationNumber: "RWFM-PU-112",
+            publicationDate: "2021-10",
+            url: "https://agrilifelearn.tamu.edu/s/product/how-to-beat-huisache/01t4x000002dp5SAAQ"
+        },
+        treatments: [
+            {
+                treatmentMethod: "leaf spray",
+                worksWellOn: "Huisache that have good leaf canopy and are less than 6ft tall.",
+                whenToApply: "Begin in September after any late-summer growth has stopped. Continue until November or when the soil temperature drops below 75F at 12in. deep. Good soil moisture and mature, healthy leaves are necessary for improved plant kill.",
+                preapreEquipment: "Backpack sprayers are the most efficient for small acreages or areas with a high density of huisache trees. Larger areas with lower densities may find ATV/UTV sprayers more efficient. An adjustable cone nozzle, such as the Conejet 5500-X6 or X8, that can deliver a coarse spray with larger droplets is needed.",
+                mixOptions: [
+                    {
+                        title: "",
+                        ingredients: []
+                    }
+                ],
+                applyTreatment: "Adjust the nozzle to deliver a coarse spray in a wide pattern. Wet all the leaves of each plant until the leaves glisten but not to the point of dripping.",
+                notes: ["Follow herbicide label instructions.", "Do not spray when new groth is present, leaves are wet or damaged, or when working upwind of desirable plants.", "The cost of treatment rises rapidly as the brush becomes biger and denser.", "Controlling huisache is not a one-time job."]
+            },
+
+            {
+                treatmentMethod: "stem spray",
+                worksWellOn: "For controlling young seedlings or older trees with three or fewer basal stems.",
+                whenToApply: "Anytime during the year, best results occur during the growing season.",
+                preapreEquipment: "Backpack sprayers are the most effecient wat to apply the stem spray to many trees. Make sure the sprayer's nozzle has a small orifice. One example is the Conejet 5500-X1 (or X2).",
+                mixOptions: [
+                    {
+                        title: "",
+                        ingredients: []
+                    }
+                ],
+                applyTreatment: "Adjust the sprayer nozzle to dleiver a narrow, cone-hsped mist. Spray the mixture lightly but evenly aon all the plant stems from te ground line up to 12 inches. Apply to all sides of every stem, but do not wet it so much that it runs off the stem and puddles.",
+                notes: ["Follow the herbicide label directions.", "The cost of treatment escalates rapidly as brush becomes denser or the number of basal stems per plant increases.", "Multi-stemmed or rough-barked plants are more difficult to control with this method.", "Do not spray when the basal stems are wet.", "After mixing the herbicide with diesel, shake or agitate the solution vigorously.", "The method is more difficult to apply if there is dense grass around the basal stems."]
+            },
+        ],
+    },
+
+    {
         plantName: [""],
         source: {
             title: "",
@@ -39,6 +80,375 @@ export const brushBustersRecomendations = [
             },
         ],
     },
+
+    {
+        plantName: [""],
+        source: {
+            title: "",
+            publicationNumber: "",
+            publicationDate: "YYYY-MM",
+            url: ""
+        },
+        treatments: [
+            {
+                treatmentMethod: "",
+                worksWellOn: "",
+                whenToApply: "",
+                preapreEquipment: "",
+                mixOptions: [
+                    {
+                        title: "",
+                        ingredients: []
+                    }
+                ],
+                applyTreatment: "",
+                notes: []
+            },
+
+            {
+                treatmentMethod: "",
+                worksWellOn: "",
+                whenToApply: "",
+                preapreEquipment: "",
+                mixOptions: [
+                    {
+                        title: "",
+                        ingredients: []
+                    }
+                ],
+                applyTreatment: "",
+                notes: []
+            },
+        ],
+    }, 
+
+    {
+        plantName: [""],
+        source: {
+            title: "",
+            publicationNumber: "",
+            publicationDate: "YYYY-MM",
+            url: ""
+        },
+        treatments: [
+            {
+                treatmentMethod: "",
+                worksWellOn: "",
+                whenToApply: "",
+                preapreEquipment: "",
+                mixOptions: [
+                    {
+                        title: "",
+                        ingredients: []
+                    }
+                ],
+                applyTreatment: "",
+                notes: []
+            },
+
+            {
+                treatmentMethod: "",
+                worksWellOn: "",
+                whenToApply: "",
+                preapreEquipment: "",
+                mixOptions: [
+                    {
+                        title: "",
+                        ingredients: []
+                    }
+                ],
+                applyTreatment: "",
+                notes: []
+            },
+        ],
+    }, 
+
+    {
+        plantName: [""],
+        source: {
+            title: "",
+            publicationNumber: "",
+            publicationDate: "YYYY-MM",
+            url: ""
+        },
+        treatments: [
+            {
+                treatmentMethod: "",
+                worksWellOn: "",
+                whenToApply: "",
+                preapreEquipment: "",
+                mixOptions: [
+                    {
+                        title: "",
+                        ingredients: []
+                    }
+                ],
+                applyTreatment: "",
+                notes: []
+            },
+
+            {
+                treatmentMethod: "",
+                worksWellOn: "",
+                whenToApply: "",
+                preapreEquipment: "",
+                mixOptions: [
+                    {
+                        title: "",
+                        ingredients: []
+                    }
+                ],
+                applyTreatment: "",
+                notes: []
+            },
+        ],
+    }, 
+
+    {
+        plantName: [""],
+        source: {
+            title: "",
+            publicationNumber: "",
+            publicationDate: "YYYY-MM",
+            url: ""
+        },
+        treatments: [
+            {
+                treatmentMethod: "",
+                worksWellOn: "",
+                whenToApply: "",
+                preapreEquipment: "",
+                mixOptions: [
+                    {
+                        title: "",
+                        ingredients: []
+                    }
+                ],
+                applyTreatment: "",
+                notes: []
+            },
+
+            {
+                treatmentMethod: "",
+                worksWellOn: "",
+                whenToApply: "",
+                preapreEquipment: "",
+                mixOptions: [
+                    {
+                        title: "",
+                        ingredients: []
+                    }
+                ],
+                applyTreatment: "",
+                notes: []
+            },
+        ],
+    }, 
+
+    {
+        plantName: [""],
+        source: {
+            title: "",
+            publicationNumber: "",
+            publicationDate: "YYYY-MM",
+            url: ""
+        },
+        treatments: [
+            {
+                treatmentMethod: "",
+                worksWellOn: "",
+                whenToApply: "",
+                preapreEquipment: "",
+                mixOptions: [
+                    {
+                        title: "",
+                        ingredients: []
+                    }
+                ],
+                applyTreatment: "",
+                notes: []
+            },
+
+            {
+                treatmentMethod: "",
+                worksWellOn: "",
+                whenToApply: "",
+                preapreEquipment: "",
+                mixOptions: [
+                    {
+                        title: "",
+                        ingredients: []
+                    }
+                ],
+                applyTreatment: "",
+                notes: []
+            },
+        ],
+    }, 
+
+    {
+        plantName: [""],
+        source: {
+            title: "",
+            publicationNumber: "",
+            publicationDate: "YYYY-MM",
+            url: ""
+        },
+        treatments: [
+            {
+                treatmentMethod: "",
+                worksWellOn: "",
+                whenToApply: "",
+                preapreEquipment: "",
+                mixOptions: [
+                    {
+                        title: "",
+                        ingredients: []
+                    }
+                ],
+                applyTreatment: "",
+                notes: []
+            },
+
+            {
+                treatmentMethod: "",
+                worksWellOn: "",
+                whenToApply: "",
+                preapreEquipment: "",
+                mixOptions: [
+                    {
+                        title: "",
+                        ingredients: []
+                    }
+                ],
+                applyTreatment: "",
+                notes: []
+            },
+        ],
+    }, 
+
+    {
+        plantName: [""],
+        source: {
+            title: "",
+            publicationNumber: "",
+            publicationDate: "YYYY-MM",
+            url: ""
+        },
+        treatments: [
+            {
+                treatmentMethod: "",
+                worksWellOn: "",
+                whenToApply: "",
+                preapreEquipment: "",
+                mixOptions: [
+                    {
+                        title: "",
+                        ingredients: []
+                    }
+                ],
+                applyTreatment: "",
+                notes: []
+            },
+
+            {
+                treatmentMethod: "",
+                worksWellOn: "",
+                whenToApply: "",
+                preapreEquipment: "",
+                mixOptions: [
+                    {
+                        title: "",
+                        ingredients: []
+                    }
+                ],
+                applyTreatment: "",
+                notes: []
+            },
+        ],
+    }, 
+
+    {
+        plantName: [""],
+        source: {
+            title: "",
+            publicationNumber: "",
+            publicationDate: "YYYY-MM",
+            url: ""
+        },
+        treatments: [
+            {
+                treatmentMethod: "",
+                worksWellOn: "",
+                whenToApply: "",
+                preapreEquipment: "",
+                mixOptions: [
+                    {
+                        title: "",
+                        ingredients: []
+                    }
+                ],
+                applyTreatment: "",
+                notes: []
+            },
+
+            {
+                treatmentMethod: "",
+                worksWellOn: "",
+                whenToApply: "",
+                preapreEquipment: "",
+                mixOptions: [
+                    {
+                        title: "",
+                        ingredients: []
+                    }
+                ],
+                applyTreatment: "",
+                notes: []
+            },
+        ],
+    }, 
+
+    {
+        plantName: [""],
+        source: {
+            title: "",
+            publicationNumber: "",
+            publicationDate: "YYYY-MM",
+            url: ""
+        },
+        treatments: [
+            {
+                treatmentMethod: "",
+                worksWellOn: "",
+                whenToApply: "",
+                preapreEquipment: "",
+                mixOptions: [
+                    {
+                        title: "",
+                        ingredients: []
+                    }
+                ],
+                applyTreatment: "",
+                notes: []
+            },
+
+            {
+                treatmentMethod: "",
+                worksWellOn: "",
+                whenToApply: "",
+                preapreEquipment: "",
+                mixOptions: [
+                    {
+                        title: "",
+                        ingredients: []
+                    }
+                ],
+                applyTreatment: "",
+                notes: []
+            },
+        ],
+    }, 
 
     {
         plantName: [""],
